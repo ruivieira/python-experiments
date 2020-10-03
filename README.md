df87445aedb688c55eca67ed6ce6b8c06e9bc197
Rui Vieira <ruidevieira@googlemail.com>
2020-10-03 14:36:53 +0100

    Add graduated projects

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 
 # python-experiments [![builds.sr.ht status](https://builds.sr.ht/~ruivieira/python-experiments.svg)](https://builds.sr.ht/~ruivieira/python-experiments?)
 
-![snake](docs/felix.png)
+![felix](docs/felix.png)
 
 A place for Python experiments.
 
 * `readme.py`, generates this README.md
+* `core.py`, Library to fetch pre-prints from arXiv
 * `bear.py`, utility to manipulate and query Bear notes
-* `arxivist.py`, Library to fetch pre-prints from arXiv
 * `arxivstatsbot.py`, Mastodon bot announcing new arXiv stats pre-prints
+
+![felix-graduated](docs/felix-graduated.png)
+## Graduated
+* `python-ssm`, A library for State-Space Models (SSM) [[repo](https://git.sr.ht/~ruivieira/python-ssm)]
--- readme.py
+++ readme.py
@@ -2,7 +2,8 @@
 # INFO: generates this README.md
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Optional
+import json
 
 
 @dataclass
@@ -11,6 +12,7 @@ class Info:
 
     filename: str
     description: str
+    url: Optional[str]
 
 
 PROJECTS: List[Info] = []
@@ -19,7 +21,7 @@ for path in Path(".").rglob("*.py"):
     x = open(path.resolve()).read().splitlines()
     descriptions = list(filter(lambda line: line.startswith("# INFO:"), x))
     if len(descriptions) > 0:
-        info = Info(filename=path.name, description=descriptions[0][8:])
+        info = Info(filename=path.name, description=descriptions[0][8:], url=None)
         PROJECTS.append(info)
 
 # sort the projects by description size
@@ -31,13 +33,30 @@ PROJECTS_DESCRIPTION = "\n".join(
     [f"* `{p.filename}`, {p.description}" for p in PROJECTS]
 )
 
+# get the graduated projects
+PROJECTS_GRADUATED:List[Info] = []
+
+with open('graduated.json') as json_file:
+    data = json.load(json_file)
+    for p in data:
+        project = Info(p['name'], p['description'], p['url'])
+        PROJECTS_GRADUATED.append(project)
+
+PROJECTS_GRADUATED_DESCRIPTION = "\n".join(
+    [f"* `{p.filename}`, {p.description} [[repo]({p.url})]" for p in PROJECTS_GRADUATED]
+)
+
 template = f"""
 # python-experiments [![builds.sr.ht status](https://builds.sr.ht/~ruivieira/python-experiments.svg)](https://builds.sr.ht/~ruivieira/python-experiments?)
 
-![snake](docs/felix.png)
+![felix](docs/felix.png)
 
 A place for Python experiments.
 
-{PROJECTS_DESCRIPTION}"""
+{PROJECTS_DESCRIPTION}
+
+![felix-graduated](docs/felix-graduated.png)
+## Graduated
+{PROJECTS_GRADUATED_DESCRIPTION}"""
 
 print(template)
