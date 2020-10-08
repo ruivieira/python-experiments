0668391e45defecaf0f8a1d8d0f58f458ebe0d21
Rui Vieira <ruidevieira@googlemail.com>
2020-10-08 22:45:41 +0100

    Add docs

--- intrplate.py
+++ intrplate.py
@@ -32,3 +32,11 @@ def basis_spline(data, size: int = 100, degree: int = 3, periodic: bool = False)
     values = np.linspace(periodic, (n_points - degree), size)
 
     return np.array(interpolate.splev(values, (knot_vector, data.T, degree))).T
+
+
+def noisy_basis_spline(
+    data, size: int = 100, degree: int = 3, periodic: bool = False, std: float = 1.0
+):
+    interpolated = basis_spline(data, size, degree, periodic)
+    noise = np.random.normal(0, std, interpolated.shape)
+    return interpolated + noise
