770d0f9bb4c3d399a8f29d221f8d04985ff44b40
Rui Vieira <ruidevieira@googlemail.com>
2020-10-15 22:31:09 +0100

    Create parrot class

--- parrot.py
+++ parrot.py
@@ -1,40 +1,53 @@
 """Markov chain inspired tweets"""
 # INFO: Markov chain inspired tweets
 import configparser
+from typing import List
 
 import markovify  # type: ignore
 import tweepy  # type: ignore
 
-config = configparser.ConfigParser()
-config.read("twitter.tokens")
 
-api_key = config["secrets"]["api_key"]
-api_secret = config["secrets"]["api_secret"]
-consumer_token = config["secrets"]["consumer_token"]
-consumer_secret = config["secrets"]["consumer_secret"]
+class Parrot:
+    """Class to generate fake tweets"""
+
+    def __init__(self, api: tweepy.API):
+        self._api = api
+        self._data: List[str] = []
+
+    def train_user(self, username: str, number: int) -> None:
+        """Train the Markov model using a certain user's timeline"""
+        public_tweets = self._api.user_timeline(id=username, count=number)
+
+        for parrot_tweet in public_tweets:
+            body = parrot_tweet.text
+            if not body.startswith("RT "):
+                self._data.extend(body.split("\n"))
+
+    def generate(self, number: int) -> List[str]:
+        """Generate fake tweets"""
+        _model = markovify.Text(". ".join(self._data))
+        result = []
+        for _ in range(number):
+            parrot_tweet = _model.make_short_sentence(280)
+            if parrot_tweet:
+                result.append(parrot_tweet)
+        return result
 
 
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(consumer_token, consumer_secret)
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("twitter.tokens")
 
-api = tweepy.API(auth)
+    api_key = config["secrets"]["api_key"]
+    api_secret = config["secrets"]["api_secret"]
+    consumer_token = config["secrets"]["consumer_token"]
+    consumer_secret = config["secrets"]["consumer_secret"]
 
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(consumer_token, consumer_secret)
 
-public_tweets = api.user_timeline(id="ruimvieira", count=1000)
-
-text = []
-
-for tweet in public_tweets:
-    body = tweet.text
-    if not body.startswith("RT "):
-        text.extend(body.split("\n"))
-
-# Build the model.
-text_model = markovify.Text(". ".join(text))
-
-# # Print five randomly-generated sentences
-for i in range(20):
-    tweet = text_model.make_sentence()
-    if tweet:
-        print("=" * 80)
-        print(tweet)
+    parrot = Parrot(api=tweepy.API(auth))
+    parrot.train_user(username="ruimvieira", number=1000)
+    tweets = parrot.generate(20)
+    for tweet in tweets:
+        print(tweets)
